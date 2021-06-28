2a5c2d30205a301b310814ffcb1bdbb1adc98599
Matt Dyer <madmatt1220@gmail.com>
2021-06-28 17:42:15 -0600

    initial commit

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void printHello();
+
+void printHello(){
+   printf("Hello World!\n");
+}
+
+int main() {
+   
+   int number = 10;
+   void (*pointer)();
+   
+   pointer = &printHello;
+   
+   *pointer();
+   
+   printf("pointer: %x\n", pointer);
+   
+   return 0;
+}
+
